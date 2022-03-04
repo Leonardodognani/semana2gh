53d77ebf0faef0fda5c57d905d7d3f395cf6fb65
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2022-03-04 14:22:59 -0300

    what to do and studing

---
+++ initial.sql
@@ -0,0 +1,55 @@
+/*
+Fundamentos Banco de dados	
+Create Table, Alter Table, Create View, Alter View, Foreign Keys	DDL
+
+Insert, Select, Delete, Update	DML
+
+Join, Left Join, Subselects	DML
+
+
+
+
+Fazer um sistema de locadora ou algo em que possa encaixar os conceitos acima.
+*/
+
+SELECT - extracts data from a database
+UPDATE - updates data in a database
+DELETE - deletes data from a database
+INSERT INTO - inserts new data into a database
+CREATE DATABASE - creates a new database
+ALTER DATABASE - modifies a database
+CREATE TABLE - creates a new table
+ALTER TABLE - modifies a table
+DROP TABLE - deletes a table
+CREATE INDEX - creates an index (search key)
+DROP INDEX - deletes an index
+
+
+/*
+Atualmente estudando pelo tutorial do W3school e em pouco tempo estarei criando tabela de 
+exercícios aqui.
+*/
+
+O que já foi lido:
+
+MySQL Tutorial
+MySQL HOME
+MySQL Intro
+MySQL RDBMS
+
+MySQL SQL
+MySQL SELECT
+MySQL WHERE
+MySQL AND, OR, NOT
+MySQL ORDER BY
+MySQL INSERT INTO
+MySQL NULL Values
+MySQL UPDATE
+MySQL DELETE
+MySQL LIMIT
+MySQL MIN and MAX
+MySQL COUNT, AVG, SUM
+MySQL LIKE
+MySQL Wildcards
+MySQL IN
+MySQL BETWEEN
